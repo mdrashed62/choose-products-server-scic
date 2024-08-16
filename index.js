52487aa5f73502de10c208da1f2cbbbb9b0a3cce
rashed62 <mdrashedbiswas83@gmail.com>
2024-08-16 08:27:23 +0600

    server is okay

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('product server is running')
+})
+
+app.listen(port, () => {
+    console.log(`server is running on port: ${port}`)
+})
